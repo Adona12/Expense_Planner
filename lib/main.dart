7f4c53b2ac0f6498b8da85e3ec5c01160e714785
Adona Tesfaye <adonatesfaye99@gmail.com>
2020-07-03 16:21:05 +0300

    styled the transaction items

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import './transaction.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -13,11 +15,14 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-
-
-  final List<Transaction> transaction=[
-    Transaction(id: "t1",amount: 69.99,title: "New shoes",date: DateTime.now()),
-    Transaction(id: "t2",amount: 16.75,title: "weekly Groceries",date: DateTime.now()),
+  final List<Transaction> transactions = [
+    Transaction(
+        id: "t1", amount: 69.99, title: "New shoes", date: DateTime.now()),
+    Transaction(
+        id: "t2",
+        amount: 16.75,
+        title: "weekly Groceries",
+        date: DateTime.now()),
   ];
   @override
   Widget build(BuildContext context) {
@@ -28,7 +33,7 @@ class MyHomePage extends StatelessWidget {
       body: Column(
         children: <Widget>[
           Container(
-            height:100 ,
+            height: 100,
             width: double.infinity,
             child: Card(
               elevation: 10,
@@ -38,15 +43,55 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            height: 400,
-            width: double.infinity,
-            child: Card(
-              color:Colors.indigo,
-                child: Text(
-              "List for TX",
-            )),
-          ),
+          Column(
+            children: transactions.map((transaction) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: Text(
+                        transaction.amount.toString(),
+                        style: TextStyle(
+                          color: Colors.purple,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          transaction.title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
+                        Text(
+                          transaction.date.toString(),
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 10,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
+          )
         ],
       ),
     );
